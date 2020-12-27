2b6296a7e504e3a296b444bbee6a4d6cd85100c7
RoAsad <rohailasd@gmail.com>
2020-12-27 02:34:55 -0800

    login connected with database

--- Server/server_SportsHood/src/main/java/net/javaguides/springboot/controller/SecurityConfig.java
+++ Server/server_SportsHood/src/main/java/net/javaguides/springboot/controller/SecurityConfig.java
@@ -2,16 +2,34 @@ package net.javaguides.springboot.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
 
-	public void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
 			.antMatcher("/**").authorizeRequests()
-			.antMatchers("/").permitAll()
+			.antMatchers("/", "/api/v1/").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.oauth2Login();
 	}
+//	 @Override
+//	    protected void configure(HttpSecurity http) throws Exception {
+//	    	// @formatter:off
+//	        http
+//	            .authorizeRequests(a -> a
+//	                .antMatchers("/", "/error", "/webjars/**").permitAll()
+//	                .anyRequest().authenticated()
+//	            )
+//	            .exceptionHandling(e -> e
+//	                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//	            )
+//	            .oauth2Login();
+//	        // @formatter:on
+//	    }
+
 }
