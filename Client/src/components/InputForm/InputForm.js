4e371fc2c1b87a2818824e3c732dbb438e42742d
Oxana Howard <ovtyutcheva@gmail.com>
2021-01-10 16:50:06 -0800

    Cleaned InputForm.js

--- Client/src/components/InputForm/InputForm.js
+++ Client/src/components/InputForm/InputForm.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
-import Modal from "../Modal/Modal";
-// import Select from "react-select";
-// import sports from "../Sports.json";
-// import cities from "../Cities.json";
 import SelectSport from "./SelectSport";
-// import SelectCity from "./SelectCity";
 import GoogleMap from "./GoogleMap";
 
 // STYLING COMPONENTS
@@ -31,91 +26,6 @@ const Form = styled.form`
     justify-items: center;
   }
 `;
-
-const InputSport = styled.div`
-  /* padding: 0.6rem;
-  font-size: 1rem;
-  border: none;
-  background: white;
-  width: 260px;
-  margin-top: 20px;
-
-  .css-1okebmr-indicatorSeparator {
-  } */
-
-  input {
-    padding: 0.6rem;
-    font-size: 1rem;
-    border: rgb(211, 208, 208) solid 0.5px;
-    background: white;
-    width: 240px;
-    border-radius: 3px;
-    color: rgb(211, 208, 208);
-    font-family: "Poppins", sans-serif;
-  }
-  ::placeholder {
-    color: rgb(211, 208, 208);
-  }
-
-  @media screen and (max-width: 960px) {
-    width: 70vw;
-    input {
-      width: 68vw;
-    }
-
-    ::placeholder {
-      text-align: center;
-    }
-  }
-
-  @media screen and (max-width: 600px) {
-    width: 80vw;
-    padding: 0.3rem;
-    font-size: 2rem;
-
-    input {
-      width: 78vw;
-      padding: 0.5rem;
-      font-size: 2rem;
-    }
-
-    ::placeholder {
-      text-align: center;
-      font-size: 1.3rem;
-    }
-  }
-`;
-
-// const InputCity = styled.input`
-//   padding: 0.5rem;
-//   font-size: 2rem;
-//   border: none;
-//   background: white;
-//   width: 200px;
-
-//   ::placeholder {
-//     color: rgb(211, 208, 208);
-//   }
-
-//   @media screen and (max-width: 960px) {
-//     width: 60vw;
-
-//     ::placeholder {
-//       text-align: center;
-//     }
-//   }
-
-//   @media screen and (max-width: 600px) {
-//     width: 80vw;
-//     padding: 0.4rem;
-//     font-size: 2rem;
-
-//     ::placeholder {
-//       text-align: center;
-//       font-size: 1.3rem;
-//     }
-//   }
-// `;
 
 const BtnGetMatch = styled.button`
   padding: 0.5rem;
